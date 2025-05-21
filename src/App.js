2b5b10111fc09375bce3f0ec7bde87c4950766d2
phugia63 <ndrapheal2003@gmail.com>
2025-05-21 17:20:19 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './components/home/homeSection';
+
+function App() {
+  return (
+    
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/:code" element={<Home />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/middleware/TextSlider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TexSlider.css';
+
+const TextTicker = () => {
+  return (
+    <div className="ticker-container bg-dark text-danger py-2">
+      <div className="ticker-content">
+        NHẬN NGAY THƯỞNG ĐĂNG KÝ HÀNG TRIỆU ĐỒNG! &nbsp; • &nbsp;
+        THAM GIA SÂN CHƠI CÁ CƯỢC UY TÍN NHẤT VIỆT NAM &nbsp; • &nbsp;
+        CHƠI NGAY & THẮNG LỚN! &nbsp; • &nbsp;
+        NHẬN ƯU ĐÃI ĐỘC QUYỀN!
+      </div>
+    </div>
+  );
+};
+
+export default TextTicker;
